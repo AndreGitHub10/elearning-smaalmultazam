3ecbf95661b82a3e8cde3212e9f4e9da505c7239
andre <aansah474@gmail.com>
2024-05-06 11:40:51 +0700

    Done: PraktekBaikGuru

--- app/Models/PraktekBaikGuru.php
+++ app/Models/PraktekBaikGuru.php
@@ -10,4 +10,19 @@ class PraktekBaikGuru extends Model
     use HasFactory;
     protected $table = 'praktek_baik_guru';
     protected $primaryKey = 'id_praktek_baik_guru';
+    protected $connection = 'mysql';
+
+    public static function getPraktekBaikGuruPaginate(){
+		return PraktekBaikGuru::selectRaw('id_praktek_baik_guru as id_berita, isi, judul, gambar')
+			->where('status', true)
+			->orderBy('id_praktek_baik_guru', 'DESC')
+			->paginate(8);
+	}
+
+	public static function getPraktekBaikGuruDetail($id) {
+		return PraktekBaikGuru::selectRaw('id_praktek_baik_guru as id_berita, isi, judul, gambar')
+            ->where('id_praktek_baik_guru',$id)
+			->where('status', true)
+			->first();
+	}
 }
